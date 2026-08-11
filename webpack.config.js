d496c855ea17297baf01f828adb81a9f289f188a
sergey060614 <janbaev.sergej@gmail.com>
2026-08-11 23:02:03 +0500

    fix: hardcode publicPath to /goblin-game/ in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const PUBLIC_PATH = '/goblin-game/';
 
 module.exports = {
   mode: 'development',
@@ -9,30 +11,21 @@ module.exports = {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: 'assets/[hash][ext][query]',
-    publicPath: '/'
+    publicPath: PUBLIC_PATH
   },
   devServer: {
     static: path.join(__dirname, 'dist'),
     port: 8080,
     open: true,
-    hot: true
+    hot: true,
+    historyApiFallback: true
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+  module: { /* ... */ },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      base: publicUrl 
     }),
   ],
 };
